0b61751c505285e380431d3817848e099143913d
Daniel <dan.sleepo@gmail.com>
2022-02-16 07:12:54 +0300

    Ravesli 147

---
+++ s4/ravesli147/Creature.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include "./Creature.h"
+#include "./Point2D.h"
+
+Creature::Creature(const std::string& name, const Point2D &location): m_name(name), m_location(location)
+{
+}
+
+std::ostream& operator<<(std::ostream& out, const Creature &creature)
+{
+    out << creature.m_name << " is at " << creature.m_location;
+    return out;
+}
+
+void Creature::moveTo(int x, int y)
+{
+    m_location.setPoint(x, y);
+}
